95dccbf346f6e73ba0826028b2bd97cee8a49da6
Chris Greenway <chris.greenway@esg-usa.com>
2017-08-30 11:05:38 -0400

    styling

--- webscr/scra/src/cljs/scra/core.cljs
+++ webscr/scra/src/cljs/scra/core.cljs
@@ -4,14 +4,6 @@
             [cljs.core.async :refer [<!]])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
-;;=====FOR INCLUDING FROM REPL=====
-;;(def nodemodulesloc "/resources/public/js/node_modules/")
-;;(defn fullpath [modname]
-;;  (str (.cwd nodejs/process) nodemodulesloc modname))
-;;
-;;(set! js/XMLHttpRequest (nodejs/require (fullpath "xhr2")))
-;;(set! js/cheerio (nodejs/require (fullpath "cheerio")))
-
 (set! js/XMLHttpRequest (nodejs/require "xhr2"))
 (set! js/cheerio (nodejs/require "cheerio"))
 
@@ -22,15 +14,15 @@
 (defn domfrom [bodystring]
   (.load js/cheerio bodystring))
 
-(println "Goodbye marky")
-
 (defn getarr [cheerobj]
   (let [length (aget cheerobj "length")]
     (loop [x 0
            goodarr []]
-      (if (= x length)
+      (if (= x length) 
         (identity goodarr)
-        (recur (inc x) (conj goodarr (.text (js/cheerio (aget cheerobj x)))))))))
+        (recur (inc x) 
+               (conj goodarr 
+                     (.text (js/cheerio (aget cheerobj x)))))))))
 
 (go (let [resp (<! (http/get "http://reddit.com"))]   
       (let [dom (domfrom (:body resp))]
